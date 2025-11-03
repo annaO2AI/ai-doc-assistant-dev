54aa8d80e7330fc3d52e6726925c25cfb6009a6a
Sravani-Ravulla <Ravula.Sravani@elevancesystems.com>
2025-11-03 18:39:16 +0530

    Practitioner Name updated

--- app/mediNote-ai/epic/epicPatientSearch.tsx
+++ app/mediNote-ai/epic/epicPatientSearch.tsx
@@ -136,7 +136,7 @@ export default function EpicPatientSearch({
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
             <p className="text-sm text-green-700">
-              Practitioner selected: <strong>{selectedDoctor.first_name} {selectedDoctor.last_name}</strong>
+              Practitioner selected : <strong>{selectedDoctor.full_name}</strong>
             </p>
           </div>
         </div>
